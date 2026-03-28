ed1e943dbb5e0a5d93baf821e9e2be173f7040ec
Haneefah55 <olasupoomotayo@gmail.com>
2026-03-28 12:34:24 +0100

    redis updated

--- backend/utils/redis.js
+++ backend/utils/redis.js
@@ -2,7 +2,10 @@ import { Redis } from 'ioredis'
 import dotenv from 'dotenv'
 
 dotenv.config()
-export const redis = new Redis(process.env.REDIS_URL) 
+export const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+}) 
 //await client.set('foo', 'bar');
 
 
